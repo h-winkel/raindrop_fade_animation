4af886bd982d128f442e01171142d77aca4aa110
Hans <winkelhans@proton.me>
2026-06-08 07:20:12 +0900

    added example project

--- src/README.md
+++ src/README.md
@@ -1,39 +1,30 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/tools/pub/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/to/develop-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+Animates one or multiple texts/images as if they were raindrops on a body of water.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+Animate multiple texts multiple times:
+![Demo Video](https://example.com/video.mp4)
+
+<!-- #code ./example/lib/main.dart -->
+
+Animate multiple images multiple times:
+![Demo Video](https://example.com/video.mp4)
+
+Animate one text multiple times:
+![Demo Video](https://example.com/video.mp4)
+
+Animate one image multiple times:
+![Demo Video](https://example.com/video.mp4)
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+Install the package using:
+flutter pub add raindrop_fade_animation <newest version>
 
-## Usage
+Or add it manually to your project's pubspec.yaml file.
+No further dependencies neccessary.
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
+## Examples
 
-```dart
-const like = 'sample';
-```
+<!-- #code ./example/lib/main.dart -->
 
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
---
+++ src/example/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:raindrop_fade_animation/raindrop_fade_animation.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentAnimation = 0;
+
+  // #begin
+  final singleText = RaindropFadeField.text(
+    text: 'test',
+    backgroundColor: Colors.blue,
+    maxAnimationCount: 5,
+    duration: Duration(milliseconds: 1500),
+    particleSize: Size(200, 200),
+    textStyle: TextStyle(fontSize: 28, color: Colors.white),);
+
+  final multipleTexts = RaindropFadeIndividualField.texts(
+    texts: ['test', 'flutter', 'linux', 'pub.dev'],
+    backgroundColor: Colors.blue,
+    maxAnimationCount: 5,
+    duration: Duration(milliseconds: 1500),
+    particleSize: Size(200, 200),
+    textStyle: TextStyle(fontSize: 28, color: Colors.white),);
+
+  final singleImage = RaindropFadeField.image(
+    imageProvider: AssetImage('assets/flutter.png'),
+    backgroundColor: Colors.blue,
+    maxAnimationCount: 5,
+    duration: Duration(milliseconds: 1500),
+    particleSize: Size(200, 200),);
+
+  final multipleImages = RaindropFadeIndividualField.images(
+    imageProviders: [AssetImage('assets/flutter.png'), AssetImage('assets/tux.png'),],
+    backgroundColor: Colors.blue,
+    maxAnimationCount: 5,
+    duration: Duration(milliseconds: 1500),
+    particleSize: Size(200, 200),);
+  // #end
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Raindrop Fade Animation',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => setState(() {
+            currentAnimation >= 3
+              ? currentAnimation = 0
+              : currentAnimation++;
+          })
+        ),
+        body: SafeArea(
+          child: SizedBox.expand(
+            child: <Widget>[singleText, multipleTexts, singleImage, multipleImages][currentAnimation],
+          ),
+        ),
+      ),
+    );
+  }
+}
